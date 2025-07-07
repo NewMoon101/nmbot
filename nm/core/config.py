1c375c733323916c574d2080fd59d046cc1046d7
smalllqiang <F11F10E8@outlook.com>
2025-07-07 11:04:07 +0800

    chore: db config 稍微修改database config

--- nm/core/config.py
+++ nm/core/config.py
@@ -9,10 +9,10 @@ class ConfigNm:
     class DatabaseConfig:
         def __init__(self, db_config: dict):
             self.host = db_config.get("host", "localhost")
-            self.port = db_config.get("port", 3306)
-            self.user = db_config.get("user", "root")
-            self.password = db_config.get("password", "")
-            self.database = db_config.get("database", "ncatbot")
+            self.port = db_config.get("port", 5432)
+            self.user = db_config.get("user", "nmbot")
+            self.password = db_config.get("password", "123456")
+            self.database = db_config.get("database", "nmbot")
 
     def __init__(self, path: str):
         with open(path, mode="r", encoding="utf-8") as config_file:
