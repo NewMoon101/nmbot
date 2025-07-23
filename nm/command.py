381bf28b2721178ab1464668cab0370c9b38abeb
smalllqiang <F11F10E8@outlook.com>
2025-07-23 20:21:03 +0800

    fix: argparse发送systemexit导致退出

--- nm/command.py
+++ nm/command.py
@@ -27,19 +27,23 @@ async def command(bot: BotClient, msg: GroupMessage, config_nm: ConfigNm, logger
         parser.add_argument("command", type=str, help="要執行的命令")
         if not get_msg_text(msg).strip(): # 如果消息文本為空，則不執行任何命令; 且防止parser解析出錯導致的sysexit
             return None
-        args = parser.parse_args(shlex.split(get_msg_text(msg).strip()))
-        if len(get_msg_at(msg)) == 0 or (config_nm.selfid in get_msg_at(msg) and (len(get_msg_at(msg)) == 1)): # 如果沒被@, 直接執行; 如果被@了, 只有只@了bot纔能執行
-            if args.command == "sysinfo":
-                sysinfo = get_sysinfo()
-                logger.info(f"系統信息: {sysinfo}")
-                await bot.api.post_group_msg(group_id=msg.group_id, text=f"系統信息: {sysinfo}")
-            elif args.command == "help":
-                await bot.api.post_group_msg(group_id=msg.group_id, text="可用命令: sysinfo, help")
-            elif args.command == "update":
-                parser.add_argument("-m", "--module", type=str, help="要更新的模塊")
-                args = parser.parse_args(shlex.split(get_msg_text(msg)))
-                if args.module:
-                    if args.module == "group_info":
-                        await update_group_info(bot, group_info_db, logger)
+        try:
+            args = parser.parse_args(shlex.split(get_msg_text(msg).strip()))
+        except Exception as e:
+            return
         else:
-            pass
+            if len(get_msg_at(msg)) == 0 or (config_nm.selfid in get_msg_at(msg) and (len(get_msg_at(msg)) == 1)): # 如果沒被@, 直接執行; 如果被@了, 只有只@了bot纔能執行
+                if args.command == "sysinfo":
+                    sysinfo = get_sysinfo()
+                    logger.info(f"系統信息: {sysinfo}")
+                    await bot.api.post_group_msg(group_id=msg.group_id, text=f"系統信息: {sysinfo}")
+                elif args.command == "help":
+                    await bot.api.post_group_msg(group_id=msg.group_id, text="可用命令: sysinfo, help")
+                elif args.command == "update":
+                    parser.add_argument("-m", "--module", type=str, help="要更新的模塊")
+                    args = parser.parse_args(shlex.split(get_msg_text(msg)))
+                    if args.module:
+                        if args.module == "group_info":
+                            await update_group_info(bot, group_info_db, logger)
+            else:
+                pass
