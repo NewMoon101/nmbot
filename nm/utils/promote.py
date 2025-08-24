e5e4de39b34d69ef0efe40023fc74baa193bab11
smalllqiang <F11F10E8@outlook.com>
2025-08-24 20:23:28 +0800

    fix: 捕獲可能存在的問題

--- nm/utils/promote.py
+++ nm/utils/promote.py
@@ -217,6 +217,11 @@ async def promote_t(bot: BotClient, msg: GroupMessage, config_nm: ConfigNm, logg
     promote_config: PromoteConfig = config_nm.promote_config # type: ignore
     for group_id in group_list:
         await sleep_random_async(promote_config.promote_time_min, promote_config.promote_time_max)
-        await bot.api.forward_group_single_msg(message_id=promote_msg_id, group_id=group_id)
-        logger.info(f"(bot:{config_nm.selfid}) 消息{promote_msg_id} 已宣發至群{group_id}")
+        try:
+            await bot.api.forward_group_single_msg(message_id=promote_msg_id, group_id=group_id)
+        except Exception as e:
+            logger.error(f"(bot:{config_nm.selfid}) {e}")
+            logger.error(f"(bot:{config_nm.selfid}) 消息{promote_msg_id} 宣發至群{group_id} 時失敗")
+        else:
+            logger.info(f"(bot:{config_nm.selfid}) 消息{promote_msg_id} 已宣發至群{group_id}")
     await bot.api.post_group_msg(group_id=promote_post_group_id, text="宣发成功", reply=promote_msg_id)
